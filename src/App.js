4ff06dd255739d6921c1bbdc60be0652c6f6d7d8
ReginaSekarm <rsekarmelati@gmail.com>
2026-03-25 11:01:51 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Regina from './pages/regina';
+import Nilam from './pages/nilam';
+import Sinta from './pages/sinta';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/regina" element={<Regina />} />
+      <Route path="/nilam" element={<Nilam />} />
+      <Route path="/sinta" element={<Sinta />} />
+    </Routes>
   );
 }
 
