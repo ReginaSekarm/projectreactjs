a4e092a914f49007f80d305f9b1d25323ac134f1
ReginaSekarm <rsekarmelati@gmail.com>
2026-03-29 18:42:50 +0700

    tambah halaman utama

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import { Routes, Route } from 'react-router-dom';
 import Regina from './pages/regina';
 import Nilam from './pages/nilam';
 import Sinta from './pages/sinta';
-import Home from './pages/Home'; 
+import Home from './pages/home';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} /> 
+      <Route path="/" element={<Home />} />
       <Route path="/regina" element={<Regina />} />
       <Route path="/nilam" element={<Nilam />} />
       <Route path="/sinta" element={<Sinta />} />
